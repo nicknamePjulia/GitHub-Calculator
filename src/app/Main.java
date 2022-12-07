63b05d26110280ac547732794bd9fa5bd7f9226c
nicknamePjulia <kulini4.julia@gmail.com>
2022-12-07 10:16:09 +0100

    Add multiplication

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,11 +8,17 @@ public class Main {
         int a = 8;
         int b = 2;
         int c = add(a, b);
-
         System.out.println(c);
+
+        int d = multiply(a, b);
+        System.out.println(d);
     }
 
     private static int add(int a, int b){
         return a + b;
+    }
+
+    private static int multiply(int a, int b){
+        return a * b;
     }
 }
